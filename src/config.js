f2a13e83941229b547992d12ecd5f785b2c9ad5a
hezihe <hezihe@xbonline.net>
2018-05-23 11:26:07 +0800

    fix: update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,17 @@
 const hello = "world"
 const say = "This is a sunny day"
+async function timeout(ms) {
+  await new Promise((resolve)=>{
+  console.log('start');
+  setTimeout(resolve, ms);
+})}
+
+async function print(value, ms) {
+  await timeout(ms);
+  console.log(value);
+}
+
+print('hello world', 2000).then(()=>{
+  print('another async', 1000);
+  console.log('then');
+})
